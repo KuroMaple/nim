081efd8f7857386517947dc2db751e8a4738fa5d
Hassan Hashmi <hashmihassan47@gmail.com>
2025-01-27 01:24:36 -0500

    custom font and title text

--- nim-frontend/src/App.tsx
+++ nim-frontend/src/App.tsx
@@ -8,9 +8,13 @@ function App() {
         <img
           src={backgroundImage}
           alt="Game Background"
-          className="w-full h-full object-fill"
+          className="w-full h-full object-fill absolute pointer-events-none"
         />
-        {/* Your game content */}
+        <div
+          className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-36'
+        >
+          <h1 className="text-9xl font-montserrat text-figma-brown">Nim</h1>
+        </div>
       </div>
     </>
   )
